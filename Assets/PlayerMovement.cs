f6ad68b1188d12f6dda3ad98c168220ca2e6fe72
suwandi-amin-sangaji <suwandiaminsangaji@gmail.com>
2021-10-07 23:22:17 +0900

    Problem-2

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public int health = 3;
+    public float moveSpeed;
+    private Vector3 targetMove;
+
+    [SerializeField] GameObject[] heartImage;
+
+    private Rigidbody2D p_rigidbody;
+
+    private void Awake()
+    {
+        p_rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        SetHealthUI();
+    }
+
+    private void Update()
+    {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        float yInput = Input.GetAxisRaw("Vertical");
+        targetMove = new Vector2(xInput, yInput).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        p_rigidbody.velocity = targetMove * moveSpeed;
+    }
+
+    public void TakeDamage()
+    {
+        health--;
+        SetHealthUI();
+        if(health == 0)
+        {
+            GameManager.Instance.GameOver();
+            Destroy(gameObject);
+        }
+    }
+
+    private void SetHealthUI()
+    {
+        for (int i = 0; i < heartImage.Length; i++)
+        {
+            if(i < health)
+            {
+                heartImage[i].SetActive(true);
+            }
+            else
+            {
+                heartImage[i].SetActive(false);
+            }
+        }
+    }
+}
