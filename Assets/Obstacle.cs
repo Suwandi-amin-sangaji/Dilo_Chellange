8ec6e87042852a12fc78c693db463d0c80b628c7
suwandi-amin-sangaji <suwandiaminsangaji@gmail.com>
2021-10-07 23:47:37 +0900

    Problem-9

---
+++ Assets/Obstacle.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    private float moveSpeed;
+    private Rigidbody2D rigidBody;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (transform.position.x > 10 || transform.position.x < -10)
+            gameObject.SetActive(false);
+        if (transform.position.y > 10 || transform.position.y < -10)
+            gameObject.SetActive(false);
+    }
+
+    public void MoveTo(Vector2 direction)
+    {
+        moveSpeed = Random.Range(5, 8);
+        rigidBody.velocity = direction.normalized * moveSpeed;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.GetComponent<PlayerMovement>().TakeDamage();
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    #region SINGLETON
+    private static ScoreManager _instance;
+    public static ScoreManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<ScoreManager>();
+                if (_instance == null) Debug.LogError("No scoremanager instance found");
+            }
+            return _instance;
+        }
+    }
+    #endregion
+
+    private static int highScore;
+    private int currentScore;
+
+    public int HighScore { get { return highScore; } }
+    public int CurrentScore { get { return currentScore; } }
+
+    public Text scoreText;
+    public Text scoreTextOnGameOver;
+    public Text highScoreText;
+
+    private void Start()
+    {
+        ResetScore();
+        if (PlayerPrefs.HasKey("Highscore"))
+        {
+            highScore = PlayerPrefs.GetInt("Highscore");
+        }
+    }
+
+    private void ResetScore()
+    {
+        currentScore = 0;
+        scoreText.text = $"{currentScore}";
+    }
+
+    public void AddScore()
+    {
+        currentScore++;
+        scoreText.text = $"{currentScore}";
+    }
+
+    public void SetHighScore()
+    {
+        if(currentScore >= highScore)
+        {
+            highScore = currentScore;
+            PlayerPrefs.SetInt("Highscore", highScore);
+        }
+
+        scoreTextOnGameOver.text = $"Score : {currentScore}";
+        highScoreText.text = $"Highscore : {highScore}";
+    }
+}
